a517470d6fb468960a65d776cea5bf2eb542bc22
mwgh <81459366+mwgh@users.noreply.github.com>
2022-06-15 13:21:05 -0700

    Update 11A.py
    indent with 4 spaces

--- cscircles/11A.py
+++ cscircles/11A.py
@@ -1,10 +1,10 @@
 def lowerChar(char):
-   if 65 <= ord(char) <= 90:
-      return chr(ord(char) + 32)
-   return char
+    if 65 <= ord(char) <= 90:
+        return chr(ord(char) + 32)
+    return char
 
 def lowerString(string):
-   result = ""
-   for i in range(0,len(string)):
-      result = result + lowerChar(string[i])
-   return result
+    result = ""
+    for i in range(0,len(string)):
+        result = result + lowerChar(string[i])
+    return result
