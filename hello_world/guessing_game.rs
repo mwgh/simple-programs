0a616fe998c2ff7ef2bcdfcc2af6282fa1b84ba5
mwgh <81459366+mwgh@users.noreply.github.com>
2023-04-05 11:17:36 -0700

    Update guessing_game.rs

--- hello_world/guessing_game.rs
+++ hello_world/guessing_game.rs
@@ -5,11 +5,11 @@ fn main() {
 
     println!("Please input your guess.");
 
-	let mut guess = String::new();
+    let mut guess = String::new();
 
-	io::stdin()
-		.read_line(&mut guess)
-		.expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-	println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess);
 }
