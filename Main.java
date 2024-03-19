bb141d675348c23ad7b8921f9409e8648b7316cb
mwgh <81459366+mwgh@users.noreply.github.com>
2024-03-18 17:04:46 -0700

    It is reading from stdin instead of file

---
+++ Main.java
@@ -0,0 +1,29 @@
+// 12751 https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=4604
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int numCases = 0, numMarbles, conseqMarbles, startingMarble; // T, N, K, X
+        double total, subtract;
+        int diff; // remaining weight
+
+        Scanner in = new Scanner(System.in);
+        if (in.hasNextInt()) {
+            numCases = in.nextInt();
+        }
+
+        // numCases (T) < 100
+        for (int i = 0; i < numCases; i++) {
+            numMarbles = in.nextInt();
+            conseqMarbles = in.nextInt();
+            startingMarble = in.nextInt();
+                
+            total = (numMarbles/2.0) * (numMarbles + 1);
+            subtract = (conseqMarbles/2.0) * ((2 * startingMarble) + conseqMarbles - 1);
+            diff = (int) (total - subtract);
+
+            System.out.println("Case " + (i + 1) + ": " + diff);
+        }
+    }
+}
