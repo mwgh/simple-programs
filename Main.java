cb1e2f16988781f1fbdf1e08b70d86aa8f602799
undefined <undefined>
2024-03-19 22:18:38 -0700

    Retab 4 space -> 1 tab

--- Main.java
+++ Main.java
@@ -3,27 +3,27 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
-        int numCases = 0, numMarbles, conseqMarbles, startingMarble; // T, N, K, X
-        double total, subtract;
-        int diff; // remaining weight
+	public static void main(String[] args) {
+		int numCases = 0, numMarbles, conseqMarbles, startingMarble; // T, N, K, X
+		double total, subtract;
+		int diff; // remaining weight
 
-        Scanner in = new Scanner(System.in);
-        if (in.hasNextInt()) {
-            numCases = in.nextInt();
-        }
+		Scanner in = new Scanner(System.in);
+		if (in.hasNextInt()) {
+			numCases = in.nextInt();
+		}
 
-        // numCases (T) < 100
-        for (int i = 0; i < numCases; i++) {
-            numMarbles = in.nextInt();
-            conseqMarbles = in.nextInt();
-            startingMarble = in.nextInt();
-                
-            total = (numMarbles/2.0) * (numMarbles + 1);
-            subtract = (conseqMarbles/2.0) * ((2 * startingMarble) + conseqMarbles - 1);
-            diff = (int) (total - subtract);
+		// numCases (T) < 100
+		for (int i = 0; i < numCases; i++) {
+			numMarbles = in.nextInt();
+			conseqMarbles = in.nextInt();
+			startingMarble = in.nextInt();
 
-            System.out.println("Case " + (i + 1) + ": " + diff);
-        }
-    }
+			total = (numMarbles/2.0) * (numMarbles + 1);
+			subtract = (conseqMarbles/2.0) * ((2 * startingMarble) + conseqMarbles - 1);
+			diff = (int) (total - subtract);
+
+			System.out.println("Case " + (i + 1) + ": " + diff);
+		}
+	}
 }
--- leetcode/Trie.java
+++ leetcode/Trie.java
@@ -1,90 +1,90 @@
 class Trie {
-    private TrieNode root;
+	private TrieNode root;
 
-    class TrieNode {
-        private static final int ALPHABET_SIZE = 26;
-        private TrieNode[] children = new TrieNode[ALPHABET_SIZE];
-        private boolean isEnd;
+	class TrieNode {
+		private static final int ALPHABET_SIZE = 26;
+		private TrieNode[] children = new TrieNode[ALPHABET_SIZE];
+		private boolean isEnd;
 
-        public TrieNode get(char c) {
-            return children[c - 'a'];
-        }
+		public TrieNode get(char c) {
+			return children[c - 'a'];
+		}
 
-        public void put(char c, TrieNode node) {
-            children[c - 'a'] = node;
-        }
+		public void put(char c, TrieNode node) {
+			children[c - 'a'] = node;
+		}
 
-        public boolean isEnd() {
-            return isEnd;
-        }
+		public boolean isEnd() {
+			return isEnd;
+		}
 
-        public void setEnd(boolean isEnd) {
-            this.isEnd = isEnd;
-        }
-    }
+		public void setEnd(boolean isEnd) {
+			this.isEnd = isEnd;
+		}
+	}
 
-    public Trie() {
-        root = createNode();
-    }
+	public Trie() {
+		root = createNode();
+	}
 
-    public static void main(String[] args) {
-        String key = "word";
-        String prefix = "wi";
+	public static void main(String[] args) {
+		String key = "word";
+		String prefix = "wi";
 
-        Trie obj = new Trie();
-        obj.insert(key);
-        boolean param_2 = obj.search(key);
-        boolean param_3 = obj.startsWith(prefix);
-        System.out.println("Inserted '" + key + "' in the trie");
-        System.out.println("Found the key '" + key + "' in the trie: " + param_2);
-        System.out.println("Found the prefix '" + prefix + "' in the trie: " + param_3);
-    }
+		Trie obj = new Trie();
+		obj.insert(key);
+		boolean param_2 = obj.search(key);
+		boolean param_3 = obj.startsWith(prefix);
+		System.out.println("Inserted '" + key + "' in the trie");
+		System.out.println("Found the key '" + key + "' in the trie: " + param_2);
+		System.out.println("Found the prefix '" + prefix + "' in the trie: " + param_3);
+	}
 
-    public void insert(String word) {
-        TrieNode current = root;
+	public void insert(String word) {
+		TrieNode current = root;
 
-        for (int i = 0; i < word.length(); ++i) {
-            char c = word.charAt(i);
-            TrieNode next = current.get(c);
+		for (int i = 0; i < word.length(); ++i) {
+			char c = word.charAt(i);
+			TrieNode next = current.get(c);
 
-            if (next == null) {
-                next = createNode();
-                current.put(c, next);
-            }
-            current = next;
-        }
+			if (next == null) {
+				next = createNode();
+				current.put(c, next);
+			}
+			current = next;
+		}
 
-        current.setEnd(true);
-    }
+		current.setEnd(true);
+	}
 
-    public boolean search(String word) {
-        TrieNode node = searchPrefix(word);
-        return node != null && node.isEnd();
-    }
+	public boolean search(String word) {
+		TrieNode node = searchPrefix(word);
+		return node != null && node.isEnd();
+	}
 
-    public boolean startsWith(String prefix) {
-        return searchPrefix(prefix) != null;
-    }
+	public boolean startsWith(String prefix) {
+		return searchPrefix(prefix) != null;
+	}
 
-    private TrieNode searchPrefix(String word) {
-        TrieNode current = root;
+	private TrieNode searchPrefix(String word) {
+		TrieNode current = root;
 
-        for (int i = 0; i < word.length(); ++i) {
-            TrieNode next = current.get(word.charAt(i));
-            
-            if (next != null) {
-                current = next;
-            } else {
-                return null;
-            }
-        }
+		for (int i = 0; i < word.length(); ++i) {
+			TrieNode next = current.get(word.charAt(i));
 
-        return current;
-    }
+			if (next != null) {
+				current = next;
+			} else {
+				return null;
+			}
+		}
 
-    private TrieNode createNode() {
-        TrieNode node = new TrieNode();
-        node.setEnd(false);
-        return node;
-    }
+		return current;
+	}
+
+	private TrieNode createNode() {
+		TrieNode node = new TrieNode();
+		node.setEnd(false);
+		return node;
+	}
 }
